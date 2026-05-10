030a50a7a7854e0195ade2f5beadc3fcc395fd71
Amanda Lima <142536832+amandaklima@users.noreply.github.com>
2026-05-10 17:25:54 +0000

    adicionando txt

--- simulador.c
+++ simulador.c
@@ -1,28 +1,63 @@
-
-// typedef struct {
-//     int id;
-//     int tempo_total;     // tempo total que o processo precisa
-//     int tempo_restante;  // quanto falta executar
-// } Processo;
-
 #include <stdio.h>
-#include <stdlib.h> 
 #include <stdlib.h>
 
-// FIFO 
-void simularFIFO(Processo fila[], int n) {
-    int tempo = 0;
+typedef struct {
+    char id[10];
+    int chegada;
+    int tempo_execucao;
+    int prioridade;
+    int tempo_restante;
+} Processo;
 
-    for (int i = 0; i < n; i++) {
-        printf("\n--- FIFO Executando %d ---\n", fila[i].id);
+// FIFO
+// void simularFIFO(Processo fila[], int n) {
 
-        while (fila[i].tempo_restante > 0) {
-            printf("Tempo %d | Processo %d executando...\n", tempo, fila[i].id);
+//     int tempo = 0;
 
-            fila[i].tempo_restante--;
-            tempo++;
-        }
+//     for (int i = 0; i < n; i++) {
 
-        printf("Processo %d finalizado no tempo %d\n", fila[i].id, tempo);
+//         printf("\n--- FIFO Executando %s ---\n", fila[i].id);
+
+//         while (fila[i].tempo_restante > 0) {
+
+//             printf("Tempo %d | Processo %s executando...\n",
+//                    tempo,
+//                    fila[i].id);
+
+//             fila[i].tempo_restante--;
+
+//             tempo++;
+//         }
+//         printf("Processo %s finalizado no tempo %d\n",
+//                fila[i].id,
+//                tempo);
+//     }
+// }
+
+int main() {
+
+    FILE *file;
+    Processo p[10];
+    int i = 0;
+    file = fopen("processos.txt", "r");
+
+    if (file == NULL) {
+        printf("Erro ao abrir arquivo\n");
+        return 1;
     }
+
+    while (fscanf(file,
+                  "%s %d %d %d %d",
+                  p[i].id,
+                  &p[i].chegada,
+                  &p[i].tempo_execucao,
+                  &p[i].prioridade,
+                  &p[i].tempo_restante) == 5) {
+
+        i++;
+    }
+
+    fclose(file);
+    printf("Processos carregados: %d\n", i);
+    return 0;
 }
